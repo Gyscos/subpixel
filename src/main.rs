20370522fd98c7daef14ae952af4031af44f9a19
Alexandre Bury <alexandre.bury@gmail.com>
2015-12-12 00:41:34 -0800

    Slightly cleaner code

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,60 @@ use std::path::Path;
 
 use image::GenericImage;
 
-enum Pattern {
-    RGB,
+trait Pattern {
+    fn size(&self) -> u32;
+    fn slice(&self, rgb: &[u8; 4]) -> Vec<image::Rgba<u8>>;
 }
 
-impl Pattern {
-    fn new(string: &str) -> Self {
-        match string {
-            "rgb" | "RGB" => Pattern::RGB,
-            _ => panic!("Unrecognized pattern: `{}`", string),
-        }
-    }
-
+// Currently only one pattern is implemented: RGB
+struct RGB;
+impl Pattern for RGB {
     fn size(&self) -> u32 {
-        match self {
-            &Pattern::RGB => 3,
-        }
+        3
     }
 
     fn slice(&self, rgb: &[u8; 4]) -> Vec<image::Rgba<u8>> {
-        match self {
-            &Pattern::RGB => {
-                let r = image::Rgba{
-                    data: [rgb[0],0,0,rgb[3]],
-                };
-                let g = image::Rgba{
-                    data: [0,rgb[1],0,rgb[3]],
-                };
-                let b = image::Rgba{
-                    data: [0,0,rgb[2],rgb[3]],
-                };
+        let r = image::Rgba{
+            data: [rgb[0],0,0,rgb[3]],
+        };
+        let g = image::Rgba{
+            data: [0,rgb[1],0,rgb[3]],
+        };
+        let b = image::Rgba{
+            data: [0,0,rgb[2],rgb[3]],
+        };
 
-                // We return a square, three times the same row: r,g,b
-                vec![r,g,b,
-                     r,g,b,
-                     r,g,b]
-            },
-        }
+        // We return a square, three times the same row: r,g,b
+        vec![r,g,b,
+             r,g,b,
+             r,g,b]
     }
+}
 
+fn parse_pattern(string: &str) -> Box<Pattern> {
+    Box::new(match string {
+        "RGB" | "rgb" => RGB,
+        _ => panic!("Unrecognized pattern: {}", string),
+    })
 }
 
 fn main() {
     // Skip exe name
     let mut args = env::args();
-    args.next().unwrap();
+    let binary = args.next().unwrap();
+
+    // First mandatory argument is the input image filename
     let filename = match args.next() {
         None => {
-            panic!("No file given");
+            panic!("No file given. Usage: {} FILE", binary);
         },
         Some(text) => text,
     };
 
-    let pattern = match args.next() {
-        None => Pattern::RGB,
-        Some(text) => Pattern::new(&text),
+    // Second optionnal argument
+    let pattern: Box<Pattern> = match args.next() {
+        None => Box::new(RGB),
+        Some(text) => parse_pattern(&text),
     };
 
     println!("Loading file {}", filename);
@@ -67,6 +66,7 @@ fn main() {
 
     let (w,h) = source.dimensions();
 
+    // This is a simple progress indicator: show dots.
     let max_dots = 20;
     let dot_every = (w / max_dots * h) as usize;
 
@@ -76,17 +76,20 @@ fn main() {
     let h = h * size;
 
 
-    println!("Creating blank canvas");
+    println!("Creating blank canvas: {} x {}", w, h);
     let mut target = image::ImageBuffer::new(w, h);
 
     println!("Iterating");
     for (k,(x,y,pixel)) in source.pixels().enumerate() {
 
+        // Scale from source to target coordinates
         let x = x * size;
         let y = y * size;
 
+        // result is a size*size square of pixels, in row-major.
         let result = pattern.slice(&pixel.data);
 
+        // Blitz it into the target image.
         for i in 0..size {
             let x = x+i;
             for j in 0..size {
@@ -96,11 +99,14 @@ fn main() {
             }
         }
 
+        // Trivial progress indicator
         if k % dot_every == 0 {
             print!(".");
         }
     }
     println!("");
+
+    // Aaand we're done.
     let target_filename = format!("{}.subs.png", filename);
     println!("Now saving image as {}", target_filename);
 
